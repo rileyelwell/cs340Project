cccfe34e973d126699928e8cfd0c7be9cde1395b
rileyelwell <41974599+rileyelwell@users.noreply.github.com>
2022-05-21 19:52:10 -0700

    deleted file

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_elwellr',
-  password        : 'D9qpYwc6556Fkvb',
-  database        : 'cs340_elwellr'
-});
-module.exports.pool = pool;
