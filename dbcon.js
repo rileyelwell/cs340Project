3344c8f4070cfb17d87393cdb5d33384d8d89603
rileyelwell <41974599+rileyelwell@users.noreply.github.com>
2022-05-30 18:32:48 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_elwellr',
-  password        : 'D9qpYwc6556Fkvb',
+  user            : '##',
+  password        : '##',
   database        : 'cs340_elwellr'
 });
 module.exports.pool = pool;
